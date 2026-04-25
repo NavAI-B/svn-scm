699cab548cc0bf1be138efc668d7e0cddf9921ee
NavAI-B <1633182290@qq.com>
2026-04-26 04:13:43 +0800

    修复遗漏更换的测试配置

--- src/test/testUtil.ts
+++ src/test/testUtil.ts
@@ -178,7 +178,7 @@ export function destroyAllTempPaths() {
 
 export function activeExtension() {
   return new Promise<void>((resolve, reject) => {
-    const extension = extensions.getExtension("johnstoncode.svn-scm");
+    const extension = extensions.getExtension("NavAI-B.svn-scm");
     if (!extension) {
       reject();
       return;
